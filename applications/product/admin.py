b57207873abd9d3d7cf1e73236b39c0f4e9f77d5
Adilet0301 <osmonalievadilet10@gmail.com>
2022-04-22 18:46:21 +0600

    added comment

--- applications/product/admin.py
+++ applications/product/admin.py
@@ -1,9 +1,17 @@
 from django.contrib import admin
 
-from applications.product.models import Category, Product, Image
+from applications.product.models import Category, Product, Image, Comment
 
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'post', 'created', 'active')
+    list_filter = ('active', 'created', 'updated')
+    search_fields = ('name', 'email', 'body')
+
+
+admin.site.register(Comment)
 admin.site.register(Category)
-admin.site.register(Product)
+# admin.site.register(Product)
 admin.site.register(Image)
 
 
@@ -11,3 +19,12 @@ class ImageInAdmin(admin.TabularInline):
     model = Image
     fields = ('image', )
     max_num = 5
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    inlines = [
+        ImageInAdmin
+    ]
+
+
